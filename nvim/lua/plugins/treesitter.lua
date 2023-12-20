9eaa44d7169f18553c9c668e6f4e51f86717383e
Bruce <swiftie13s@163.com>
2023-12-20 10:36:59 +0800

    add nvim

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "cpp",
+        "fish",
+        "git_rebase",
+        "gitcommit",
+        "gn",
+        "go",
+        "gomod",
+        "gosum",
+        "html",
+        "javascript",
+        "json",
+        "json5",
+        "kdl",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "regex",
+        "rust",
+        "toml",
+        "typescript",
+        "yaml",
+      },
+    },
+  },
+}
